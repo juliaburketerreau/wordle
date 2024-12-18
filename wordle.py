cbea8c24788c518cb97a0c5c443ba05efa78213d
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-12-18 15:31:24 +0000

    starting guesses

--- wordle.py
+++ wordle.py
@@ -5,7 +5,15 @@ print("You have 6 tries to guess the 5 letter word")
 print("Please enter your words below")
 a = filehandle.readlines()
 word = random.choice(a)
-guess = input()
+for numguess in range (1,7):  
+  guess = input().upper()
+  if guess == a:
+    print("Congratulations!")
+    break
+  else:
+    continue
+
+
 
 
 
