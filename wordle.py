6edc0727164592a202c1915ade8a5471ed25f016
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-12-18 15:24:39 +0000

    adding in user input

--- wordle.py
+++ wordle.py
@@ -1,8 +1,15 @@
 import random
 filehandle = open("words.txt", 'r')
 print("Welcome to Wordle!")
-print("You have 5 tries to guess a 5 letter word")
-
+print("You have 6 tries to guess the 5 letter word")
+print("Please enter your words below")
 a = filehandle.readlines()
-print(random.choice(a))
+word = random.choice(a)
+guess = input()
+
+
+
+
+
+
 
--- words.py
+++ words.py
@@ -1,5 +1,5 @@
 filehandle = open("words.txt", 'w')
-for _ in range(5):
+for _ in range(20):
     words = str(input())
     filehandle.write(words+"\n")
 filehandle.close()
