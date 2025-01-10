01d23470ff003afcc48b23dc44c0aa83bdc3cbe8
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2025-01-10 14:58:48 +0000

    g

--- wordle.py
+++ wordle.py
@@ -8,7 +8,8 @@ print("download 'pip install rich' in the terminal before starting")
 print("You have 6 tries to guess the 5 letter word")
 print("Please enter your words below")
 a = filehandle.readlines()
-word = random.choice(a)
+#word = random.choice(a)
+word = "answe"
 counter = 0
 attempt = True
 while attempt:
@@ -16,17 +17,16 @@ while attempt:
   counter = counter + 1
   if counter == 7 and guess != word:
     print("You did not get the wordle. The wordle was...", word)
-    break
+    attempt = False
   for i in range (min(len(guess), 5)):
       if guess[i] == word[i]:
         print("[green]" + (guess[i]) + "[/green]", end="")
       elif guess [i] in word:
         print("[yellow]" + (guess[i]) + "[/yellow]", end="")
-      elif guess == word:
-        print("[blue]\n" + "Congratulations! You got the wordle!" + "[/blue]\n", end="")
-        attempt = False
       else:
         print(guess[i], end="")
-      attempt = True
-
+      
+      if guess == word:
+        print("[blue]" + "Congratulations! You got the wordle!" + "[/blue]", end="")
+        break
     
