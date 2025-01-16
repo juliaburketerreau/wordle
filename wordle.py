98d0bc1aecb735810256fcb26259c0a70d443c91
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2025-01-16 14:52:26 +0000

    h

--- wordle.py
+++ wordle.py
@@ -12,7 +12,8 @@ print("if the letter is white, the letter is not in the word")
 print("You have 6 tries to guess the 5 letter word")
 print("Please enter your words below")
 a = filehandle.readlines()
-word = random.choice(a)
+word = "tests"
+#word = random.choice(a)
 counter = 0
 attempt = True
 while attempt:
@@ -20,7 +21,7 @@ while attempt:
   counter = counter + 1
   if guess == word:
       print("[blue]" + "Congratulations! You got the wordle in " + str(counter) + " guess/guesses!" + "[/blue]", end="")
-      attempt = False
+      attempt = True
   if guess != word and counter == 6:
     print("You did not get the wordle. The wordle was...", word)
     attempt = False
