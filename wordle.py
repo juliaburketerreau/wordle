c0059a35a015b8090be7e5ef1d04ca4b35a351f4
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2025-01-17 15:10:33 +0000

    added an error checking loop for number by using try

--- wordle.py
+++ wordle.py
@@ -16,19 +16,23 @@ word = random.choice(a)
 counter = 0
 attempt = True
 while attempt:
-  guess = input('\n').lower()     
-  counter = counter + 1
-  if guess == word:
-    print("[blue]" + "Congratulations! You got the wordle in " + str(counter) + " guess/guesses!" + "[/blue]", end="")
-    attempt = True
-    
-  elif guess != word and counter == 6:
-    print("You did not get the wordle. The wordle was...", word)
-    attempt = False
-  for i in range (min(len(guess), 5)):
-      if guess[i] == word[i]:
-        print("[green]" + (guess[i]) + "[/green]", end="")
-      elif guess [i] in word:
-        print("[yellow]" + (guess[i]) + "[/yellow]", end="")
-      else:
-        print(guess[i], end="")
+  guess = (input('\n').lower()) 
+  try:
+    int(guess)
+    print('invaild guess')
+  except:
+    counter = counter + 1
+    if guess == word:
+      print("[blue]" + "Congratulations! You got the wordle in " + str(counter) + " guess/guesses!" + "[/blue]", end="")
+      break
+      
+    elif guess != word and counter == 6:
+      print("You did not get the wordle. The wordle was...", word)
+      attempt = False
+    for i in range (min(len(guess), 5)):
+        if guess[i] == word[i]:
+          print("[green]" + (guess[i]) + "[/green]", end="")
+        elif guess [i] in word:
+          print("[yellow]" + (guess[i]) + "[/yellow]", end="")
+        else:
+          print(guess[i], end="")
