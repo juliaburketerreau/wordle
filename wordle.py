6b51d7f0937c21f6d1ae27bb715546c88f487c95
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2025-01-22 14:49:46 +0000

    added comments

--- wordle.py
+++ wordle.py
@@ -2,6 +2,7 @@
 import random
 from rich import print
 from rich.color import Color
+#instructions
 filehandle = open("words.txt", 'r')
 print("Welcome to Wordle!")
 print("download 'pip install rich' in the terminal before starting")
@@ -11,10 +12,13 @@ print("if the letter turns [yellow] yellow [/yellow], the letter [blue]is[/blue]
 print("if the letter is white, the letter is [red]not[/red] in the word")
 print("You have 6 tries to guess the 5 letter word")
 print("Please enter your words below")
+#reads the file, then chooses a random word using an array, that word is now the wordle
 a = filehandle.readlines()
 word = random.choice(a).strip()
+#sets counter for number of tries
 counter = 0
 attempt = True
+#this loop begins with error checking, and then it checks if the word is correct, or if it has been more than 6 guesses and the word is still incorrect
 while attempt:
   guess = (input('\n').lower()) 
   try:
@@ -29,7 +33,9 @@ while attempt:
     elif guess != word and counter == 6:
       print("You did not get the wordle. The wordle was...", word)
       attempt = False
-    for i in range (min(len(guess), 5)):
+#this loop is the most important, and it changes the colour of the letters. 
+#it checks to see if the letters match up (green), if the letter is in the word (yellow) and if the letter is not in word (gray)
+      for i in range (min(len(guess), 5)):
         if guess[i] == word[i]:
           print("[green]" + (guess[i]) + "[/green]", end="")
         elif guess [i] in word:
